9923af26196fc0db706b209404dda335d907f4c6
Kate Hudson <k88hudson@gmail.com>
2020-10-07 20:09:33 -0400

    fix version

--- src/lib/generate-from-input.ts
+++ src/lib/generate-from-input.ts
@@ -17,7 +17,7 @@ export interface UserInput {
 
 export function renderDesktopTargetingString(userInput: UserInput) {
   const minVersion = userInput.customMinVersion || userInput.minVersion;
-  let targetingString = `env.version|versionCompare('${minVersion}') <= 0`;
+  let targetingString = `version|versionCompare('${minVersion}') <= 0`;
   if (userInput.channel) {
     targetingString += ` && browserSettings.update.channel == '${userInput.channel}'`;
   }
